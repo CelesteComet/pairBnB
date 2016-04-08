0b155511fe8ba72f3db6bd67f4e635d99371dc88
johnsonkcs <johnsonkcs@gmail.com>
2016-04-08 12:28:43 +0800

    Done Single Listing Picture Feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407094952) do
+ActiveRecord::Schema.define(version: 20160408025507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160407094952) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "listingpic"
   end
 
   create_table "profiles", force: :cascade do |t|
