ce201dfe16210e5cc4dae84c0b90d9139a5f317c
johnsonkcs <johnsonkcs@gmail.com>
2016-04-12 16:46:51 +0800

    added price to listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408084012) do
+ActiveRecord::Schema.define(version: 20160412035645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160408084012) do
     t.datetime "updated_at"
     t.string   "name"
     t.json     "listingpics"
+    t.integer  "price_per_night"
   end
 
   create_table "profiles", force: :cascade do |t|
