1c98a3cf9796ea59ed421da41035249ebf12f38a
johnsonkcs <johnsonkcs@gmail.com>
2016-04-18 12:35:58 +0800

    made images responsive, made table responsive, change uploader resize to fill for mid listing pic, searchbar responsive still unsolved

--- app/uploaders/listingpic_uploader.rb
+++ app/uploaders/listingpic_uploader.rb
@@ -36,11 +36,11 @@ class ListingpicUploader < CarrierWave::Uploader::Base
   end
 
   version :mid do
-    process :resize_to_fit => [400, 400]
+    process :resize_to_fill => [426, 275]
   end
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fill => [50, 50]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
